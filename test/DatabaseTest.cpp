00779668ac610a30c1b181cd5d8e5a1c06795851
PookyFan <pookyfan@kancolle.pl>
2024-11-16 13:33:35 +0100

    Added basic test for ProductCategory insertion through Database object

--- test/DatabaseTest.cpp
+++ test/DatabaseTest.cpp
@@ -7,16 +7,37 @@ using namespace testing;
 
 namespace FG::data::test
 {
-// template<typename T>
-// using EntityVector = std::vector<EntityPtr<T>>;
-
 struct DatabaseTestFixture : public Test
 {
-    Database db;
+    void assertProductCategoriesAreEqual(const ProductCategory& lhs, const ProductCategory& rhs)
+    {
+        ASSERT_EQ(lhs.name, rhs.name);
+        ASSERT_EQ(lhs.imagePath, rhs.imagePath);
+        ASSERT_EQ(lhs.isArchived, rhs.isArchived);
+    }
+
+    Database db{};
 };
 
+struct ProductCategoryDatabaseTestFixture : DatabaseTestFixture, public WithParamInterface<ProductCategory>
+{};
+
+TEST_P(ProductCategoryDatabaseTestFixture, DatabaseShouldCreateProductCategoryAccordingToCreateMethodParameters)
+{
+    auto templCat = GetParam();
+    EntityPtr<ProductCategory> newCat = db.create<ProductCategory>(templCat.name, templCat.imagePath, templCat.isArchived);
+    assertProductCategoriesAreEqual(templCat, *newCat);
+}
+
+INSTANTIATE_TEST_SUITE_P(ProductCategoryTest, ProductCategoryDatabaseTestFixture, Values(
+    ProductCategory{ .name = "cat1", .imagePath = "path/to/image1", .isArchived = true },
+    ProductCategory{ .name = "cat2", .imagePath = "path/to/image2", .isArchived = false },
+    ProductCategory{ .name = "cat3", .imagePath = std::nullopt, .isArchived = true },
+    ProductCategory{ .name = "cat4", .imagePath = std::nullopt, .isArchived = false }
+));
+
 template<typename T>
-struct TypedDatabaseTestFixture : public DatabaseTestFixture
+struct TypedDatabaseTestFixture : DatabaseTestFixture
 {};
 
 using EntitiesTypes = Types<ProductCategory, ProductDescription, ProductInstance>;
